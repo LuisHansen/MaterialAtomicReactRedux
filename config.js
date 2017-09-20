fe3fbcbf530e96f6e6eb9e34e9f354183e5ed500
Luis <luis.hansen@usp.br>
2017-09-20 12:26:05 -0300

    Mudança no ip

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
 	host: 'http://169.57.131.86:4040', //localhost:4040
 	today: '03/10/2017', //new Date()
-	thisHost: 'http://169.57.131.86' //'http://192.168.50.5:3000' //localhost:3000
+	thisHost: 'http://169.57.131.86:80' //'http://192.168.50.5:3000' //localhost:3000
 }
 
 module.exports = config;
